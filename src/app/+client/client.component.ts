60629f922f83d7ca09967b612dfe9b1fea9b2d80
Jonathan Jayet <jonathan@artus.io>
2016-06-16 23:19:54 +0200

    Added firebase

---
+++ src/app/+client/client.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'client',
+    templateUrl: 'client.component.html'
+})
+export class ClientComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { ProtoprintAppComponent, environment } from './app/';
+import { ROUTER_PROVIDERS } from '@angular/router';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(ProtoprintAppComponent);
+bootstrap(ProtoprintAppComponent,
+  [ROUTER_PROVIDERS,
+    FIREBASE_PROVIDERS,
+    defaultFirebase({
+      apiKey: "AIzaSyDta4PPiRdBOK23uB-KhgaoTCvSR7PBjNY",
+      authDomain: "project-733431008235281507.firebaseapp.com",
+      databaseURL: "https://project-733431008235281507.firebaseio.com",
+      storageBucket: "",
+    })]);
 
